29d9839620b2b9c27d3e560c8905caff260eef33
Iamouma <stephenouma452@gmail.com>
2023-11-09 11:38:09 -0800

    More files

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define BUFFER_SIZE 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
