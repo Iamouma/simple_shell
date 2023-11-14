d03cd5073508b7339cb0f148c522ea4e3a6e34a0
Iamouma <stephenouma452@gmail.com>
2023-11-13 23:06:15 -0800

    Binary file

--- binary_func.c
+++ binary_func.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
  * df - picks integer value and calls function to print.
  * @args: list of arguments.
- * Return: length of string.
+ * @Return: length of integer.
  */
 int df(va_list args)
 {
@@ -11,69 +10,6 @@ int df(va_list args)
 	long int n;
 
 	n = print_dif(len, 0);
-
-	return (n);
-}
-
-/**
- * print_dif - prints signed integers.
- * @len: integer to be printed.
- * @n: length of integer to be printed.
- * Return: length of integer.
- */
-int print_dif(long int len, long int n)
-{
-	if (len < 0)
-	{
-		_putchar('-');
-		len = -len;
-		n++;
-	}
-	if (len / 10)
-	{
-		n = print_dif(len / 10, n++);
-	}
-	_putchar(len % 10 + '0');
-	n++;
-
-	return (n);
-}
-
-/**
- * dcc - picks integer value and calls function to print.
- * @args: list of arguments.
- * Return: length of integer.
- */
-int dcc(va_list args)
-{
-	long int len = va_arg(args, int);
-	long int n;
-
-	n = print_dig_c(len, 0);
-
-	return (n);
-}
-
-/**
- * print_dig_c - prints unsigned integers.
- * @len: integer to be printed.
- * @n: length of integer.
- * Return: length of integer.
- */
-int print_dig_c(long int len, long int n)
-{
-	if (len < 0)
-	{
-		len = -len;
-		n++;
-	}
-
-	if (len / 10)
-	{
-		n = print_dig_c(len / 10, n++);
-	}
-	_putchar(len % 10 + '0');
-	n++;
 
 	return (n);
 }
